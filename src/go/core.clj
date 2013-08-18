b42fa3a8c83cd1a8e1607f9ee8c65b9a75110702
glts <676c7473@gmail.com>
2013-08-18 10:36:08 +0200

    Initial commit

---
+++ src/go/core.clj
@@ -0,0 +1,64 @@
+(ns go.core
+  (:import [java.net URL]
+           [java.io IOException]
+           [java.util.concurrent Executors ExecutorCompletionService TimeUnit]))
+
+(def urls {"Go" "http://golang.org/"
+           "Python" "http://python.org/"
+           "Perl" "http://www.perl.org/"
+           "Scala" "http://www.scala-lang.org/"
+           "Clojure" "http://clojure.org/"
+           "Haskell" "http://www.haskell.org/haskellwiki/Haskell"
+           "Ruby" "http://www.ruby-lang.org/en/"})
+
+(defn format-time [t] (str (format "%.3f" t) "s"))
+
+(defn fetch [lang urlstr]
+  (try
+    (let [start (System/nanoTime)
+          size (count (slurp (.openStream (URL. urlstr))))
+          t (format-time (/ (- (System/nanoTime) start) 1e9))]
+      (str lang " " size " [" t "]"))
+    (catch IOException e (println (.getMessage e)))))
+
+(defn go-1 []
+  (try
+    (let [f (fn [[k,v]] #(println (fetch k v)))
+          fns (map f urls)]
+      (dorun (apply pcalls fns)))
+    (finally (shutdown-agents))))
+
+(defn go-2 []
+  (let [pool (Executors/newFixedThreadPool 8)
+        f (fn [[k,v]] #(println (fetch k v)))
+        fns (map f urls)
+        rets (.invokeAll pool fns)]
+    (.shutdown pool))) ; TODO timeout
+
+(defn go-3 []
+  (let [pool (Executors/newFixedThreadPool 8)
+        cs (ExecutorCompletionService. pool)
+        f (fn [[k,v]] #(fetch k v))
+        fns (map f urls)]
+    (doseq [g fns] (.submit cs g))
+    (doseq [_ fns] (println (.. cs take get)))
+    (.shutdown pool)))
+
+(defn go-4 []
+  (let [pool (Executors/newFixedThreadPool 8)
+        cs (ExecutorCompletionService. pool)
+        f (fn [[k,v]] #(fetch k v))
+        fns (map f urls)]
+    (doseq [g fns] (.submit cs g))
+    (let [end (+ (System/nanoTime) 3e9)] ; 1s
+      (loop [n (count fns)]
+        (if (and (> n 0) (< (System/nanoTime) end))
+          (do (println (.. cs take get))
+              (recur (dec n)))
+          (println "Timed out"))))
+          ; TODO timeout doesn't really work yet
+          ; make threads interruptable
+    (.shutdown pool)))
+
+(defn -main [& args]
+  (go-4))
---
+++ test/go/core_test.clj
@@ -0,0 +1,9 @@
+(ns go.core-test
+  (:require [clojure.test :refer :all]
+            [go.core :refer :all]))
+
+; FIXME learn to write tests
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

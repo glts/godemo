f43a52330b40e4f1ab034f270f59760e21090d17
David Bürgin <676c7473@gmail.com>
2013-11-23 14:24:01 +0100

    Changed name to godemo

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject go "0.1.0-SNAPSHOT"
+(defproject godemo "0.1.0-SNAPSHOT"
   :description "Go demo web crawler in Clojure"
-  :url "https://github.com/glts/go"
+  :url "https://github.com/glts/godemo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main go.core)
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]]
+  :main godemo.core)

c8ada0aba4b40e7d33eec98c78f48cd29435cc76
David Bürgin <676c7473@gmail.com>
2013-11-23 14:32:03 +0100

    Added LICENSE

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject godemo "0.1.0-SNAPSHOT"
   :description "Go demo web crawler in Clojure"
   :url "https://github.com/glts/godemo"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]]
   :main godemo.core)
